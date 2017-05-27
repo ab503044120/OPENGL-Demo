2274b365f385ca9d82562c643fa9eaa77010ea40
ab503044120 <503044120@qq.com>
2017-05-27 17:55:04 +0800

    add packer sender

---
+++ OpenGLCamera/app/src/main/java/org/huihui/openglcamera/packer/RtmpPacker.java
@@ -0,0 +1,31 @@
+package org.huihui.openglcamera.packer;
+
+import android.media.MediaCodec;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+
+public class RtmpPacker implements IPacker {
+    @Override
+    public void onVideoData(ByteBuffer bb, MediaCodec.BufferInfo bi) {
+
+    }
+
+    @Override
+    public void onAudioData(ByteBuffer bb, MediaCodec.BufferInfo bi) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ OpenGLCamera/app/src/main/java/org/huihui/openglcamera/sender/ISender.java
@@ -0,0 +1,8 @@
+package org.huihui.openglcamera.sender;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+
+public interface ISender {
+}
